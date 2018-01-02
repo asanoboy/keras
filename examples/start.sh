76481832a4f8a6c20bdb73f4c8a030b47b50c038
Yosuke Asano <y.a.personal@gmail.com>
2018-01-02 16:52:28 +0900

    Fix start.sh.

--- examples/start.sh
+++ examples/start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-docker run -it --name keras-learning-jupyter -p 8888:8888 -v $PWD:/home/jovyan/work jupyter/datascience-notebook
+NAME=keras-learning-jupyter
+
+if [ "$(docker ps -qa -f name=$NAME)" ]; then
+    docker start -i $NAME
+else
+	docker run -it --name keras-learning-jupyter -p 8888:8888 -v $PWD:/home/jovyan/work jupyter/datascience-notebook
+fi
